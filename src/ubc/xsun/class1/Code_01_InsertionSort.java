428e8975b6d31d8cace73456407708e3d64adce4
xsun <sunxin89@gmail.com>
2017-12-14 15:25:11 -0800

    sss

--- src/ubc/xsun/class1/Code_01_InsertionSort.java
+++ src/ubc/xsun/class1/Code_01_InsertionSort.java
@@ -4,7 +4,15 @@ import java.util.Arrays;
 
 public class Code_01_InsertionSort {
     public static void insertionSort(int[] arr){
+        if(arr == null || arr.length < 2){
+            return;
+        }
 
+        for(int i=0; i<arr.length-1; i++){
+            for(int j=i+1; j<arr.length; j++){
+
+            }
+        }
     }
     public static void swap(int[] arr, int i, int j) {
         arr[i] = arr[i] ^ arr[j];
